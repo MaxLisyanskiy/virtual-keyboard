3c316a359b15a0427af73fb787c20ed45cde2b79
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2023-04-30 13:55:01 +0300

    fix: change mouse events for shift and caps

--- scripts/mouseup.js
+++ scripts/mouseup.js
@@ -3,16 +3,27 @@ export function onMouseUp(e) {
     const code = $item ? $item.getAttribute('data-keycode') : '';
 
     if (code === 'ShiftLeft' || code === 'ShiftRight') {
-        $item.classList.remove('active');
         const caseDown = document.querySelectorAll('.caseDown');
         const caseUp = document.querySelectorAll('.caseUp');
         const caps = document.querySelectorAll('.caps');
         const shiftCaps = document.querySelectorAll('.shiftCaps');
 
-        caseDown.forEach((item) => item.classList.remove('hidden'));
-        caseUp.forEach((item) => item.classList.add('hidden'));
-        caps.forEach((item) => item.classList.add('hidden'));
-        shiftCaps.forEach((item) => item.classList.add('hidden'));
+        const capsLock = document.querySelector('.key__caps-lock');
+
+        if (capsLock.classList.contains('active')) {
+            $item.classList.remove('active');
+            caseDown.forEach((item) => item.classList.add('hidden'));
+            caseUp.forEach((item) => item.classList.add('hidden'));
+            caps.forEach((item) => item.classList.remove('hidden'));
+            shiftCaps.forEach((item) => item.classList.add('hidden'));
+        } else {
+            $item.classList.remove('active');
+
+            caseDown.forEach((item) => item.classList.remove('hidden'));
+            caseUp.forEach((item) => item.classList.add('hidden'));
+            caps.forEach((item) => item.classList.add('hidden'));
+            shiftCaps.forEach((item) => item.classList.add('hidden'));
+        }
     } else if (code === 'CapsLock') {
         return false;
     }
